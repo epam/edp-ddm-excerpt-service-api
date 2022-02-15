aaf72ff28e497b64db461d68b12ecaf2d1719084
Yevhen Zvarych <yevhen_zvarych@epam.com>
2022-02-15 16:08:40 +0000

    [EDP-DDM-8109] - Updated security starter version to enable CSRF

--- src/main/java/com/epam/digital/data/platform/excerpt/api/config/UnauthorizedRequestHandler.java
+++ src/main/java/com/epam/digital/data/platform/excerpt/api/config/UnauthorizedRequestHandler.java
@@ -16,9 +16,10 @@
 
 package com.epam.digital.data.platform.excerpt.api.config;
 
-import com.epam.digital.data.platform.starter.security.jwt.RestAuthenticationEntryPoint;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
@@ -26,18 +27,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class UnauthorizedRequestHandler extends RestAuthenticationEntryPoint {
+public class UnauthorizedRequestHandler implements AuthenticationEntryPoint, AccessDeniedHandler {
 
   private final HandlerExceptionResolver handlerExceptionResolver;
 
-  public UnauthorizedRequestHandler(ObjectMapper objectMapper, HandlerExceptionResolver handlerExceptionResolver) {
-    super(objectMapper);
+  public UnauthorizedRequestHandler(HandlerExceptionResolver handlerExceptionResolver) {
     this.handlerExceptionResolver = handlerExceptionResolver;
   }
 
   @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response,
-      AuthenticationException e) {
+  public void commence(
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException e) {
+    handlerExceptionResolver.resolveException(request, response, null, e);
+  }
+
+  @Override
+  public void handle(
+      HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) {
     handlerExceptionResolver.resolveException(request, response, null, e);
   }
 }
